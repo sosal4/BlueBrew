34e77c82f9ddb10796c3997898374c7f9d83faed
xakR <sak_4204@yahoo.com>
2017-09-11 17:40:16 -0500

    layout connection solved

---
+++ app/src/main/java/seniordesign/bluebrew/com/bluebrew/reading.java
@@ -0,0 +1,18 @@
+package seniordesign.bluebrew.com.bluebrew;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by xakR on 8/29/2017.
+ */
+
+public class reading extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.readings);
+    }
+}
