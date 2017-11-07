65e130bbfd9de4fe313ce3ee194bd547b1851f64
sosal4 <sosal4@hotmail.com>
2017-11-07 16:06:25 -0600

    updated website Link

--- app/src/main/java/seniordesign/bluebrew/com/bluebrew/MainActivity.java
+++ app/src/main/java/seniordesign/bluebrew/com/bluebrew/MainActivity.java
@@ -4,8 +4,10 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView text = (TextView) findViewById(R.id.text1);
+        text.setMovementMethod(LinkMovementMethod.getInstance());
+
+
     }
 
     public void nd(View view) {
--- app/src/main/java/seniordesign/bluebrew/com/bluebrew/reading.java
+++ app/src/main/java/seniordesign/bluebrew/com/bluebrew/reading.java
@@ -14,5 +14,6 @@ public class reading extends Activity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.readings);
+
     }
 }
