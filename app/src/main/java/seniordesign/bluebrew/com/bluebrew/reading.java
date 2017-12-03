7e8dfe4380ca5eb4fd59270c4dee5ae0426dc3b4
xakR <sak_4204@yahoo.com>
2017-12-03 10:39:51 -0600

    using JSOUP, backstage.class(JSON part) not in used here

--- app/src/main/java/seniordesign/bluebrew/com/bluebrew/reading.java
+++ app/src/main/java/seniordesign/bluebrew/com/bluebrew/reading.java
@@ -1,11 +1,17 @@
 package seniordesign.bluebrew.com.bluebrew;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
 
 /**
  * Created by xakR on 8/29/2017.
@@ -21,7 +27,21 @@ public class reading extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.readings);
 
-        click = (Button) findViewById(R.id.button);
+        data = (TextView)findViewById(R.id.data);
+        Button click = (Button)findViewById(R.id.button);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                new doit().execute();
+            }
+        });
+
+
+
+
+        /*click = (Button) findViewById(R.id.button);
         data = (TextView) findViewById(R.id.data);
 
         click.setOnClickListener(new View.OnClickListener() {
@@ -32,7 +52,33 @@ public class reading extends Activity {
                 process.execute();
 
             }
-        });
+        });*/
 
+    }
+
+    public class doit  extends AsyncTask<Void, Void, Void>{
+
+        String words;
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            try {
+                Document doc = Jsoup.connect("http:/192.168.42.1").get();
+
+                words = doc.text();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+        data.setText(words);
+        }
     }
 }
